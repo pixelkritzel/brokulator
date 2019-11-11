c5cfea2a1d16e13a5bb1e16e6b4a8fe90e01dd2d
Timo Zoeller <timo@pixelkritzel.de>
2019-11-11 16:09:55 +0100

    Adds mobx-state-tree and material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
-const App: React.FC = () => {
+import './styles/index.scss';
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Typography>
+          <h1>Broke u later</h1>
+        </Typography>
+      </Container>
+    </React.Fragment>
   );
 }
-
-export default App;
